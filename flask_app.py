4f714bf084d668fd6046bfc9f6000d4cbdf96a37
Prof. Manoel Gadi <mfalonso@faculty.ie.edu>
2024-04-04 10:29:17 +0200

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# HTTP - LAZY
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/search')
+def seach():
+    q = request.args.get('q') 
+    return "received query string={} <br> Because they wanted a framework that was lightweight, just like their morning coffee!".format(q)
+
+
+if __name__ == '__main__':
+    app.run()
